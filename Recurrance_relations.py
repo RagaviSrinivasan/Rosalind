621b37063b292d1eec07e8c47e2b6bfec3cf5750
RagaviSrini <rxs147930@utdallas.edu>
2017-08-01 22:59:27 -0500

    added hamming distance exercise

--- Recurrance_relations.py
+++ Recurrance_relations.py
@@ -1,16 +1,24 @@
 # ------------------------------------------------------------------------------------------------------------------
 # Rosalind problems:
-# 5) Use dynamic programming to calculate the number of rabbit pairs after n months, provided each rabbit pair
-#    reproduces after 1 month.
-#    eg: there will be 19 pairs of rabbits after 5 months if each pair produces 3 rabbits at the end of each month
+# 5) Calculate Hamming distance
+#
+# For a give sequences s and t of equal length, Hamming distance is the number of mismatched nucleotides.
 #-------------------------------------------------------------------------------------------------------------------
 
-def recur_fibo(n, k):
-   """Recursive function to
-   print Fibonacci sequence"""
-   if n <= 1:
-       return k
-   else:
-       return(recur_fibo(n-1,k) + recur_fibo(n-2, k))
+def HammingDistance():
+    with open('C:/Users/Yamini/Documents/Rosalind/Input files/05_hamming_distance.txt', 'r') as f:
+        lines = f.readlines()
+        seq1 = lines[0].rstrip()
+        seq2 = lines[1].rstrip()
+        hammingDistance = 0
 
-print recur_fibo(2, 3)
+        if (len(seq1) != len(seq2)):
+            print "Two sequences must be of equal length in order to calculate the Hamming Distance"
+            return
+        else:
+            for i in range(0,len(seq1)):
+                if seq1[i] != seq2[i]:
+                    hammingDistance += 1
+            return hammingDistance
+
+print HammingDistance()
